7c68e4d0dbe13d29ede844d4e8683281cf61f3d3
ah03ad63 <125690012+ah03ad63@users.noreply.github.com>
2024-12-20 23:20:43 +0200

    Update Feature Extraction

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,13 +1,28 @@
 import librosa
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+    
+def extract_features(file_path):
+    audio, sampling_rate = load_audio(file_path)
+    segments = split_audio(audio, sampling_rate)
+    features = []
+    for segment in segments:
+        spectrogram = compute_spectrogram(segment, sr= sampling_rate)
+        mfccs = compute_mfcc(segment, sampling_rate)
+        chroma = compute_chroma(segment, sampling_rate)
+        zcr = compute_zcr(segment)
+        rmse = compute_rmse(segment)
+        segment_features = [spectrogram, mfccs, chroma, zcr, rmse]
+        features.append(segment_features)
+    # save_features(features, save_path)
+    return features
 
-def load_audio(file_path, target_sr=16000):
+def load_audio(file_path, target_sr=22050):
     audio, sr = librosa.load(file_path, sr=target_sr)
     return audio, sr
 
 
-def split_audio(audio, sr, segment_duration=5):
+def split_audio(audio, sr, segment_duration=2):
     segment_length = sr * segment_duration
     return [audio[i:i + segment_length] for i in range(0, len(audio), segment_length)]
 
@@ -46,3 +61,5 @@ def normalize_features(features):
 
 def save_features(features, file_path):
     np.save(file_path, features)
+
+features = extract_features("C:\\Users\VICTUS\Downloads\y2mate.com - Eminem  Stan Short Version ft Dido (1) [vocals].wav")
